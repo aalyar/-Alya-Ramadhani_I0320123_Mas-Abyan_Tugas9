61acac4813b5083369246736e25c17913c3b14cc
Alya R <alyaramadhani000@gmail.com>
2021-04-26 20:35:33 +0700

    exercise

---
+++ I0320123_Exercise 9.10_Tugas 9.py
@@ -0,0 +1,12 @@
+import array
+
+# nilai awal (sebelum dibalik)
+A
+array('i', [100, -700, 300, 400, 800])
+
+# membalik urutan elemen array
+A.reverse()
+
+# nilai akhir (setelah dibalik)
+A
+array('i', [800, 400, 300, -700, 100])
