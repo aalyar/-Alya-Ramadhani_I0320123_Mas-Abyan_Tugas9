f00c861a0f8ca569e6d94689194a3b9d23dee497
Alya R <alyaramadhani000@gmail.com>
2021-04-26 20:30:53 +0700

    exercise

---
+++ I0320123_Exercise 9.8_Tugas 9.py
@@ -0,0 +1,12 @@
+import array
+
+# mengonversi listke dalam array.array
+li = [10, 20, 30, 40, 50]
+C = array.array('i')
+C.fromlist(li)
+type(C)
+
+<type 'array.array'>
+
+for nilai in C:
+    print("%d" % nilai, end='')
